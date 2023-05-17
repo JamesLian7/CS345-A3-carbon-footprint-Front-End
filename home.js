24c689adecbdc641a1a6fee29469dfcbb983cd69
JamesLian7 <jameslian1104@gmail.com>
2023-05-17 12:25:39 +1200

    imported plant 2

---
+++ home.js
@@ -0,0 +1,7 @@
+function changeImage() {
+    if (document.getElementById("imgClickAndChange").src == "pngwing.com (2).png"){
+        document.getElementById("imgClickAndChange").src = "pngwing.com (3).png";
+    } else {
+        document.getElementById("imgClickAndChange").src == "pngwing.com (2).png";
+    }
+}
