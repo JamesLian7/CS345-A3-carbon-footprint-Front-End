619075d0bd57b78c7ddfafc90dc009845aed170f
JamesLian7 <jameslian1104@gmail.com>
2023-05-17 12:53:07 +1200

    renamed png names

--- home.js
+++
@@ -1,7 +0,0 @@
-function changeImage() {
-    if (document.getElementById("imgClickAndChange").src == "pngwing.com (2).png"){
-        document.getElementById("imgClickAndChange").src = "pngwing.com (3).png";
-    } else {
-        document.getElementById("imgClickAndChange").src == "pngwing.com (2).png";
-    }
-}
