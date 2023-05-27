67348f8ebe33a8b243857db9fd51ecb70de70147
JamesLian7 <jameslian1104@gmail.com>
2023-05-27 16:17:15 +1200

    modal popup animation wrking

--- home.js
+++ home.js
@@ -1,6 +1,21 @@
-document.querySelector("#trigger-modal").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active");
-});
-document.querySelector(".popup .close-btn").addEventListener("click",function(){
-    document.querySelector("popup").classList.remove("active");
-});
+var closeBut = document.getElementsByClassName("close")[0],
+  modal = document.getElementsByClassName("modal-cont")[0],
+  cancelBut = document.getElementsByClassName("cancel")[0],
+  loginBut = document.getElementsByClassName("login")[0];
+
+//close
+function x() {
+  modal.style.display = "none";
+}
+closeBut.onclick = x;
+cancelBut.onclick = x;
+
+loginBut.onclick = function () {
+  modal.style.display = "block";
+};
+
+window.onclick = function (e) {
+  if (e.target.className === "modal-cont") {
+    e.target.style.display = "none";
+  }
+};
